1a3f1d75a071e008e9b596594ba15e4db7dd1cae
Jordan C <jordanpcasel@gmail.com>
2018-10-24 00:34:53 -0700

    added reshape back in
    added reshape back in

--- LSTM_1000.py
+++ LSTM_1000.py
@@ -13,8 +13,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 # from __future__ import print_function, division
 from keras.datasets import mnist
-# removed Reshape from below dependency - was receiving error when running
-from keras.layers import Input, Dense, Flatten, Dropout
+from keras.layers import Input, Reshape, Dense, Flatten, Dropout
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D

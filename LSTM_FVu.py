82e53a28c99d20cb20a6e1fcec4fe01f8fb74171
fvu958 <francis.nguyen.vu@gmail.com>
2018-10-24 15:23:12 -0700

    more changes

--- LSTM_FVu.py
+++ LSTM_FVu.py
@@ -1,8 +1,11 @@
-# Larger LSTM Network to Generate Text for Alice in Wonderland
-import numpy
+# Larger LSTM Network to Generate Text 
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.layers import Input, Reshape, Flatten
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
@@ -26,22 +29,18 @@ print("Total Vocab: ", n_vocab)
 seq_length = 100
 dataX = []
 dataY = []
-
 for i in range(0, n_chars - seq_length, 1):
 	seq_in = raw_text[i:i + seq_length]
 	seq_out = raw_text[i + seq_length]
 	dataX.append([char_to_int[char] for char in seq_in])
 	dataY.append(char_to_int[seq_out])
-
 n_patterns = len(dataX)
 print("Total Patterns: ", n_patterns)
 
 # reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-
+X = np.reshape(dataX, (n_patterns, seq_length, 1))
 # normalize
 X = X / float(n_vocab)
-
 # one hot encode the output variable
 y = np_utils.to_categorical(dataY)
 

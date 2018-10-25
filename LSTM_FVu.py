c05330e0ce47c31ea1c7ce8ab9c713fb3a20867a
fvu958 <francis.nguyen.vu@gmail.com>
2018-10-24 17:46:56 -0700

    list to array?

--- LSTM_FVu.py
+++ LSTM_FVu.py
@@ -34,6 +34,7 @@ for i in range(0, n_chars - seq_length, 1):
 	seq_out = raw_text[i + seq_length]
 	dataX.append([char_to_int[char] for char in seq_in])
 	dataY.append(char_to_int[seq_out])
+X = np.array(X)
 n_patterns = len(dataX)
 print("Total Patterns: ", n_patterns)
 

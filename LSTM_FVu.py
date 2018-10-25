ee20d9ac30a143fcd440e3cb6d0dd31230554468
fvu958 <francis.nguyen.vu@gmail.com>
2018-10-24 18:36:55 -0700

    guy helped me

--- LSTM_FVu.py
+++ LSTM_FVu.py
@@ -11,7 +11,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
 # load ascii text and covert to lowercase
-filename = "lyrics.txt"
+filename = "lyrics2.txt"
 raw_text = open(filename).read()
 raw_text = raw_text.lower()
 
@@ -34,12 +34,12 @@ for i in range(0, n_chars - seq_length, 1):
 	seq_out = raw_text[i + seq_length]
 	dataX.append([char_to_int[char] for char in seq_in])
 	dataY.append(char_to_int[seq_out])
-X = np.array(X)
+X = np.array(dataX)
 n_patterns = len(dataX)
 print("Total Patterns: ", n_patterns)
 
 # reshape X to be [samples, time steps, features]
-X = np.array(dataX, (n_patterns, seq_length, 1))
+X = np.reshape(X, (n_patterns, seq_length, 1))
 # normalize
 X = X / float(n_vocab)
 # one hot encode the output variable

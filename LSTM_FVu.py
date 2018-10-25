75f398a0c76cfb406733251342d9727ee2177997
fvu958 <francis.nguyen.vu@gmail.com>
2018-10-25 13:41:27 -0700

    maybe a fix?

--- LSTM_FVu.py
+++ LSTM_FVu.py
@@ -1,5 +1,6 @@
 # Larger LSTM Network to Generate Text 
 import numpy as np
+import sys
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -18,6 +19,8 @@ raw_text = raw_text.lower()
 # create mapping of unique chars to integers
 chars = sorted(list(set(raw_text)))
 char_to_int = dict((c, i) for i, c in enumerate(chars))
+int_to_char = dict((i, c) for i, c in enumerate(chars))
+
 
 # summarize the loaded data
 n_chars = len(raw_text)
@@ -34,12 +37,12 @@ for i in range(0, n_chars - seq_length, 1):
 	seq_out = raw_text[i + seq_length]
 	dataX.append([char_to_int[char] for char in seq_in])
 	dataY.append(char_to_int[seq_out])
-X = np.array(dataX)
+X = np.asarray(dataX)
 n_patterns = len(dataX)
 print("Total Patterns: ", n_patterns)
 
 # reshape X to be [samples, time steps, features]
-X = np.reshape(X, (n_patterns, seq_length, 1))
+np.reshape(X, (n_patterns, seq_length, 1))
 # normalize
 X = X / float(n_vocab)
 # one hot encode the output variable

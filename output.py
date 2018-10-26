1b83e9544943ea0a69876f35ca56d3ca21396fff
fvu958 <francis.nguyen.vu@gmail.com>
2018-10-25 20:10:43 -0700

    fixed output.py

--- output.py
+++ output.py
@@ -10,6 +10,64 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
+# load ascii text and covert to lowercase
+filename = "blyrics.txt"
+raw_text = open(filename).read()
+raw_text = raw_text.lower()
+
+# create mapping of unique chars to integers
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+# int_to_char = dict((i, c) for i, c in enumerate(chars))
+
+
+# summarize the loaded data
+n_chars = len(raw_text)
+n_vocab = len(chars)
+print("Total Characters: ", n_chars)
+print("Total Vocab: ", n_vocab)
+
+# prepare the dataset of input to output pairs encoded as integers
+seq_length = 100
+dataX = []
+dataY = []
+for i in range(0, n_chars - seq_length, 1):
+	seq_in = raw_text[i:i + seq_length]
+	seq_out = raw_text[i + seq_length]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
+n_patterns = len(dataX)
+print("Total Patterns: ", n_patterns)
+
+# reshape X to be [samples, time steps, features]
+X = np.reshape(dataX, (n_patterns, seq_length, 1))
+# normalize
+X = X / float(n_vocab)
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
+
+# define the LSTM model
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(LeakyReLU(alpha=0.2))
+model.add(BatchNormalization(momentum=0.8))
+model.add(Dropout(0.2))
+model.add(LSTM(128, return_sequences=True))
+model.add(LeakyReLU(alpha=0.2))
+model.add(BatchNormalization(momentum=0.8))
+model.add(Dropout(0.2))
+model.add(LSTM(64))
+model.add(LeakyReLU(alpha=0.2))
+model.add(BatchNormalization(momentum=0.8))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# define the checkpoint
+# filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+# checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+# callbacks_list = [checkpoint]
+
 model.load('weights-improvement-02-2.1111-bigger.hdf5')
 
 # pick a random seed
@@ -19,6 +77,7 @@ print("Seed:")
 print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
 
 # generate characters
+# Predicted_text = []
 for i in range(500):
     x = np.reshape(pattern, (1, len(pattern), 1))
     x = x / float(n_vocab)
@@ -27,6 +86,8 @@ for i in range(500):
     result = int_to_char[index]
     seq_in = [int_to_char[value] for value in pattern]
     sys.stdout.write(result)
+    # Predicted_text.append(result)
     pattern.append(index)
     pattern = pattern[1:len(pattern)]
 print("\nDone.")
+# Predicted_text
